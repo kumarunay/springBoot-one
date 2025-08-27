3d5e5d16f0f6251ff153444ecb44116a0cf525d1
unay <kumarunay@gmail.com>
2025-08-27 19:37:39 +0530

    JPA CODE

---
+++ spring-jpa-ex1/src/main/java/com/unytech/spring_jpa_ex1/SpringJpaEx1Application.java
@@ -0,0 +1,13 @@
+package com.unytech.spring_jpa_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaEx1Application.class, args);
+	}
+
+}
---
+++ spring-jpa-ex1/src/main/java/com/unytech/spring_jpa_ex1/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.unytech.spring_jpa_ex1.service;
+
+import com.unytech.spring_jpa_ex1.model.Product;
+import com.unytech.spring_jpa_ex1.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+
+        return productRepo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return productRepo.findById(id).orElse(new Product(-1));
+    }
+
+    public Product addOrUpdateProduct(Product product, MultipartFile imageFile) throws IOException {
+
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return productRepo.searchProducts(keyword);
+    }
+}
